0738997299a18b8b6dee02b75fd8ef05b8bdc633
Rashmi <rashmiap.10@gmail.com>
2018-09-07 12:22:34 +0530

    react-router-dom added added routes / - artists /albums - albums

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Artists from './Components/Artists';
+import Albums from './Components/Albums';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Artists} />
+          <Route exact path="/albums" component={Albums} />
+        </Switch>
+      </Router>
     );
   }
 }
