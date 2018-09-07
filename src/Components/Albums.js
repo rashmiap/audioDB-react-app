4648a60719e9d86e3a5986bf6659e7fa1dc215d7
Rashmi <rashmiap.10@gmail.com>
2018-09-07 12:21:24 +0530

    album and artists components created

---
+++ src/Components/Albums.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Albums extends Component {
+  render() {
+    return (
+      <div>
+      Albums
+      </div>
+    );
+  }
+}
+export default Albums;
---
+++ src/Components/Artists.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Artists extends Component {
+  render() {
+    return (
+      <div>
+      artists
+      </div>
+    );
+  }
+}
+export default Artists;
